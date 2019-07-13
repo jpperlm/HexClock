03ecd9f9e3967e180ac462e8260a66d00702139f
Jason Perlman <jasonpperlman@gmail.com>
2019-07-13 15:10:30 -0400

    Remove icons

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+// import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBars, faQuestionCircle, faArrowLeft)
+// library.add(faBars, faQuestionCircle, faArrowLeft)
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
